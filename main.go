387345d40070d5b127324a4614a766662f6bda31
Samuel Hawksby-Robinson <samuel@samyoul.com>
2024-06-12 14:16:02 +0100

    Added main loop to websocket handler
    Also added some stub functions for reading and writing socket data

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"crypto/sha1"
 	"encoding/base64"
+	"log"
+	"net"
 	"net/http"
 )
 
@@ -18,12 +21,12 @@ const (
 
 // handleWebSocket is the handler passed into a http server
 func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	// Perform WebSocket handshake
 	if r.Header.Get(HeaderUpgrade) != websocket || r.Header.Get(HeaderConnection) != HeaderUpgrade {
 		http.Error(w, "Not a WebSocket handshake", http.StatusBadRequest)
 		return
 	}
 
-	// Perform WebSocket handshake
 	secWebSocketKey := r.Header.Get(HeaderSecWebSocketKey)
 	secWebSocketAccept := computeAcceptKey(secWebSocketKey)
 
@@ -36,6 +39,7 @@ func handleWebSocket(w http.ResponseWriter, r *http.Request) {
 		w.Header()[k] = v
 	}
 
+	// Hijacking things
 	w.WriteHeader(http.StatusSwitchingProtocols)
 	hijacker, ok := w.(http.Hijacker)
 	if !ok {
@@ -43,12 +47,30 @@ func handleWebSocket(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	conn, _, err := hijacker.Hijack()
+	conn, buf, err := hijacker.Hijack()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	defer conn.Close()
+
+	// Main WebSocket loop
+	// Handle WebSocket frames
+	for {
+		// Websocket read data
+		messageType, payload, err := handleReadWebSocketData(buf)
+		if err != nil {
+			log.Println("Error reading WebSocket message:", err)
+			break
+		}
+		log.Printf("Received message: %s", payload)
+
+		// Websocket write data
+		if err := handleWriteWebSocketData(conn, messageType, payload); err != nil {
+			log.Println("Error writing WebSocket message:", err)
+			break
+		}
+	}
 }
 
 // computeAcceptKey is used to generate the `secWebSocketAccept`
@@ -57,4 +79,14 @@ func computeAcceptKey(key string) string {
 	sha1Hash := sha1.New()
 	sha1Hash.Write([]byte(key + WebsocketsMagicString))
 	return base64.StdEncoding.EncodeToString(sha1Hash.Sum(nil))
+}
+
+func handleReadWebSocketData(buf *bufio.ReadWriter) (messageType int, payload []byte, err error) {
+	// todo
+	return 0, nil, nil
+}
+
+func handleWriteWebSocketData(conn net.Conn, messageType int, payload []byte) error {
+	// todo
+	return nil
 }
